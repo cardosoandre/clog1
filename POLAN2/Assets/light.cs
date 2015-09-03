28f24e94af8ebf7d2e54020916f5496623953347
Andre Cardoso <amc.acardoso@gmail.com>
2015-09-03 17:59:21 -0400

    Today's Class

---
+++ POLAN2/Assets/light.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class light: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	// when a player walks into the trigger on this gameobjects
+	void OnTriggerEnter(Collider other) {
+	// get the light component on this game object, turn it off
+		gameObject.GetComponent<Light> ().enabled = false;
+}
+	void OnTriggerExit(Collider other) {
+		// get the light component on this game object, turn it off
+		gameObject.GetComponent<Light> ().enabled = true;
+
+}
+}
